d10c6aaa9f83a7ad2a5c0d1b0c6287aaa9ee134a
Oseaghe <oseagheebhomielen@gmail.com>
2025-08-28 14:06:31 +0100

    Initial commit

---
+++ src/main/java/org/example/cointoss/CointossApplication.java
@@ -0,0 +1,13 @@
+package org.example.cointoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CointossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CointossApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cointoss/CointossApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cointoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CointossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
