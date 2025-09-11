3d4bcd3a38bfa31074343288e80456aa17a209be
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-09-11 23:14:42 +0100

    feat: TickerResponse (may change later)

---
+++ src/main/java/org/example/cointoss/dtos/TickerResponse.java
@@ -0,0 +1,21 @@
+package org.example.cointoss.dtos;
+
+import java.math.BigDecimal;
+
+public class TickerResponse {
+    private String symbol;
+    private BigDecimal price;
+
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
